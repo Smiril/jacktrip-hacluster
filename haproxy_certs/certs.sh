9eda4036c07e5a2d8850b8d92a958b1ab612ff04
Smiril <Smiril@users.noreply.github.com>
2026-04-16 23:07:24 +0200

    Update certs.sh

--- haproxy_certs/certs.sh
+++ haproxy_certs/certs.sh
@@ -2,5 +2,5 @@
 
 openssl genrsa -out priv.key 4096
 openssl req -new -nodes -sha256 -key priv.key -out certs.csr
-openssl x509 -req -sha256 -days 3650 -in certs.csr -signkey priv.key -out certs.pem
+openssl x509 -req -sha256 -days 365 -in certs.csr -signkey priv.key -out certs.pem
 cat priv.key >> certs.pem
