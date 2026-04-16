12e3bc2a3bb5e653543a429625f41ca110ec2094
Smiril <Smiril@users.noreply.github.com>
2026-04-16 23:15:54 +0200

    Update certs.sh

--- haproxy_certs/certs.sh
+++ haproxy_certs/certs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-openssl genrsa -out priv.key 4096
-openssl req -new -nodes -sha256 -key priv.key -out certs.csr
-openssl x509 -req -sha256 -days 365 -in certs.csr -signkey priv.key -out certs.pem
-cat priv.key >> certs.pem
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days `echo 365*4|bc -l` -subj "/C=AT/ST=VIE/L=VIE/O=SELFSIGNED/CN=`hostname -f`"
+openssl rsa -in key.pem -out newkey.pem && mv key.pem key.pem.old && mv newkey.pem key.pem
+openssl dhparam -out dhparams.pem 2048
+cat key.pem cert.pem dhparams.pem > certs.pem
