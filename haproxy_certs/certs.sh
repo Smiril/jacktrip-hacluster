adeb926f97af220fbc70c9a1a8dfaf5dc9f71632
Smiril <Smiril@users.noreply.github.com>
2026-04-17 00:08:14 +0200

    Update certs.sh

--- haproxy_certs/certs.sh
+++ haproxy_certs/certs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm -f key.pem && rm -f key.pem.old && rm -f dhparams.pem && rm -f cert.pem
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days `echo 365*4|bc -l` -subj "/C=AT/ST=VIE/L=VIE/O=SELFSIGNED/CN=`hostname -f`"
 openssl rsa -in key.pem -out newkey.pem && mv key.pem key.pem.old && mv newkey.pem key.pem
 openssl dhparam -out dhparams.pem 2048
